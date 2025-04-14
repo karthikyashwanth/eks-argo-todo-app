643429485546234e46b62f4106eff14db0820737
karthikyashwanth <125976205+karthikyashwanth@users.noreply.github.com>
2025-04-14 18:30:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
                         fontWeight: "bolder",
                     }}
                 >
-                    CDIT-2-DEMO - "Done by Yashwanthk"
+                    CDIT-2-DEMO - "Done by Yashwanth"
                 </Row>
   
                 <hr />
