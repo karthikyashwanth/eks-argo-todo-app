56b803b8a8a430ddb6526826b75009646c876817
karthikyashwanth <125976205+karthikyashwanth@users.noreply.github.com>
2025-04-14 18:21:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
                         fontWeight: "bolder",
                     }}
                 >
-                    CDIT-2-DEMO - TODO LIST
+                    CDIT-2-DEMO - My App
                 </Row>
   
                 <hr />
